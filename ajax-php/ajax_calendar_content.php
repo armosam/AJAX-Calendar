7c4e7c9d2196566b1c454f0c106aa45614d9e6e8
Armen Bablanyan <armosam@users.noreply.github.com>
2015-08-18 15:56:02 -0700

    Added ID of group
    Added ID of group to filter only events of that group

--- ajax-php/ajax_calendar_content.php
+++ ajax-php/ajax_calendar_content.php
@@ -7,18 +7,24 @@
  */
 $month = filter_input(INPUT_POST, 'm', FILTER_SANITIZE_NUMBER_INT);
 $year = filter_input(INPUT_POST, 'y', FILTER_SANITIZE_NUMBER_INT);
+$id = filter_input(INPUT_POST, 'id', FILTER_SANITIZE_NUMBER_INT);
 
 $events = array();
 /*
-$sql = "SELECT events.e_id, DAY(events.e_date) as `day`, shows.short_title FROM `events` JOIN `shows` ON shows.show_id=events.e_show_id WHERE YEAR(e_date) =".$year." AND MONTH(e_date)=".$month." AND DATE(e_date) >= DATE(NOW())";
+$where_shows = '';
+if (!empty($id)) { $where_shows = " AND events.e_show_id = ".$id; }
+
+$sql = "SELECT events.e_id, DAY(events.e_date) as `day`, events.e_date FROM `events` WHERE YEAR(e_date) =".$year." AND MONTH(e_date)=".$month." AND DATE(e_date) >= DATE(NOW()) ".$where_shows;
+
 $result=$session->mysqli->query($sql);
 if($result && $result->num_rows>0){
     while($row = $result->fetch_object()){ 
-        $events[$row->day] = array('id'=>$row->e_id, 'title'=>$row->short_title);
+        $events[$row->day] = array('id'=>$id, 'title'=>date("F j, Y", strtotime($row->e_date)));
     }
-}*/
+}
 
-$events = array(23=>array(id=>5, title=>'Test Event 1'), 25=>array(id=>15, title=>'Test Event 2'));
+*/
 
+$events = array(23=>array(id=>$id, title=>'Test Event 1'), 25=>array(id=>$id, title=>'Test Event 2'));
 
 echo json_encode($events);
